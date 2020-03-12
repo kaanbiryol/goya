5adf942c97b712b45ef3d1190c74c169cfbf8fbe
Kaan Biryol <kaan.biryol@adesso.com.tr>
2020-03-12 18:16:44 +0300

    Update container_extension.dart

--- lib/src/extensions/container_extension.dart
+++ lib/src/extensions/container_extension.dart
@@ -6,7 +6,7 @@ import 'package:goya/src/theme_symbol.dart';
 
 extension ContainerExtension on Container {
   Container toGoya(Container goyaContainer) {
-    Color goyaColor = (goyaContainer.decoration as BoxDecoration).color;
+    Color goyaColor = (goyaContainer.decoration as BoxDecoration)?.color;
     return Container(
       key: key,
       alignment: alignment,
